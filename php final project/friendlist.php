2fcf92525c534ae1540790bf797e1bd5c34f606d
Aryan Ukani <63489402+aryan2434@users.noreply.github.com>
2023-08-12 00:46:50 -0400

    Add files via upload

--- php final project/friendlist.php
+++ php final project/friendlist.php
@@ -3,47 +3,42 @@
 include('./resourses/database.php');
 include('./resourses/header.php');
 
-
-
-// Fetch friend list data from the database
-$sql = "SELECT * FROM friendlist";
+// Fetch user data from the database
+$sql = "SELECT * FROM userd";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
-$friends = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
-<!-- first line of table -->
 <div class="container mt-5">
-    <h2 class="mb-4">Friend List</h2>
+    <h2 class="mb-4">User List</h2>
     <table class="table">
         <thead>
             <tr>
                 <th>ID</th>
-                <th>Name</th>
-                <th>Age</th>
+                <th>Username</th>
+                <th>Password</th> <!-- Again, note the security concern here. -->
+                <th>Email</th>
                 <th>Action</th>
             </tr>
         </thead>
         <tbody>
-            <!-- displaying table -->
-            <?php foreach ($friends as $friend) : ?>
+            <?php foreach ($users as $user) : ?>
                 <tr>
-                    <td><?= $friend['ID'] ?></td>
-                    <td><?= $friend['Name'] ?></td>
-                    <td><?= $friend['Age'] ?></td>
+                    <td><?= $user['ID'] ?></td>
+                    <td><?= $user['username'] ?></td>
+                    <td><?= $user['password'] ?></td> <!-- This should not be displayed in a real-world application -->
+                    <td><?= $user['email'] ?></td>
                     <td>
-                        <!-- btn to edit and remove friends -->
-                        <a href="editfriend.php?id=<?= $friend['ID'] ?>" class="btn btn-primary btn-sm">Edit</a>
-                        <a href="deletefriend.php?id=<?= $friend['ID'] ?>" class="btn btn-danger btn-sm">Delete</a>
+                        <a href="editfriend.php?id=<?= $user['ID'] ?>" class="btn btn-primary btn-sm">Edit</a>
+                        <a href="deletefriend.php?id=<?= $user['ID'] ?>" class="btn btn-danger btn-sm">Delete</a>
                     </td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
     </table>
-    <!-- btn for add friend and logout -->
     <div class="mt-3">
-        <a href="addfriend.php" class="btn btn-success">Add Friend</a>
+        <a href="addfriend.php" class="btn btn-success">Add User</a>
         <a href="logout.php" class="btn btn-danger">Logout</a>
     </div>
 </div>
-<!-- insert footer -->
 <?php include('./resourses/footer.php'); ?>
